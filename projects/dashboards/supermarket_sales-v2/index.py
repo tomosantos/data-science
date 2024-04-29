0388a4e69585e569016820423c2c02b35d8fb3b1
Wellinton Oliveira <wellintonoliveira04@gmail.com>
2024-04-29 11:56:53 -0300

    finish supermarket sales v2

--- projects/dashboards/supermarket_sales-v2/index.py
+++ projects/dashboards/supermarket_sales-v2/index.py
@@ -9,9 +9,17 @@ import plotly.express as px
 import plotly.graph_objects as go
 import dash_bootstrap_components as dbc
 
+from dash_bootstrap_templates import load_figure_template
+
+load_figure_template('minty')
+
 
 app = dash.Dash(
-    external_stylesheets=[dbc.themes.DARKLY]
+    external_stylesheets=[
+        dbc.themes.MINTY,
+        'https://fonts.googleapis.com/css2?family=Voltaire&display=swap'
+    ]
+    
 )
 server = app.server
 
@@ -23,15 +31,37 @@ df_data['Date'] = pd.to_datetime(df_data['Date'])
 
 # =========== Layout =========== #
 app.layout = html.Div(children=[
-    html.H5("Cidades:"),
-    dcc.Checklist(df_data['City'].value_counts().index, df_data['City'].value_counts().index, id='check-city'),
     
-    html.H5("Variável de Análise: "),
-    dcc.RadioItems(['gross income', 'Rating'], "gross income", id='main-variable'),
+    dbc.Row([
+        dbc.Col([
+            dbc.Card([
+                html.H2('ASIMOV', style={'font-family': "Voltaire", 'font-size': '55px'}),
+                html.Hr(),
+                html.H5("Cidades:"),
+                dcc.Checklist(
+                    df_data['City'].value_counts().index, 
+                    df_data['City'].value_counts().index, 
+                    id='check-city', inputStyle={'margin-right': '5px', 'margin-left': '5px'}
+                ),
+            
+                html.H5("Variável de Análise: ", style={'margin-top': '30px'}),
+                dcc.RadioItems(['gross income', 'Rating'], "gross income", id='main-variable', inputStyle={'margin-right': '5px', 'margin-left': '5px'}),
+                
+            ], style={'height': '90vh', 'margin': '20px', 'padding': '20px'})
+        ], sm=2),
+        
+        dbc.Col([
+            dbc.Row([
+                dbc.Col([dcc.Graph(id='city-fig'),], sm=4),
+                dbc.Col([dcc.Graph(id='gender-fig'),], sm=4),
+                dbc.Col([dcc.Graph(id='pay-fig'),], sm=4),
+            ]),
+            dbc.Row([dcc.Graph(id='income-per-date-fig'),]),
+            dbc.Row([dcc.Graph(id='income-per-product-fig'),]),
+                    
+        ], sm=10)
+    ])
     
-    dcc.Graph(id='city-fig'),
-    dcc.Graph(id='pay-fig'),
-    dcc.Graph(id='income-per-product-fig'),
     
 ])
 
@@ -40,6 +70,8 @@ app.layout = html.Div(children=[
 @app.callback([
         Output('city-fig', 'figure'),
         Output('pay-fig', 'figure'),
+        Output('gender-fig', 'figure'),
+        Output('income-per-date-fig', 'figure'),
         Output('income-per-product-fig', 'figure'),
     ],
     [
@@ -52,18 +84,24 @@ def render_graphs(cities, main_variable):
     df_filtered = df_data[df_data['City'].isin(cities)]
     
     df_city = df_filtered.groupby('City')[main_variable].apply(operation).to_frame().reset_index()
+    df_gender = df_filtered.groupby(['Gender', 'City'])[main_variable].apply(operation).to_frame().reset_index()
     df_payment = df_filtered.groupby(['Payment'])[main_variable].apply(operation).to_frame().reset_index()
+    
     df_product_income = df_filtered.groupby(["Product line", "City"])[main_variable].apply(operation).to_frame().reset_index()
+    df_income_time = df_filtered.groupby('Date')[main_variable].apply(operation).to_frame().reset_index()
 
     fig_city = px.bar(df_city, x='City', y=main_variable)
     fig_payment = px.bar(df_payment, y='Payment', x=main_variable, orientation='h')
+    fig_gender = px.bar(df_gender, x='Gender', y=main_variable, color="City", barmode='group')
     fig_product_income = px.bar(df_product_income, x=main_variable, y="Product line", color="City", orientation="h", barmode='group')
+    fig_income_date = px.bar(df_income_time, y=main_variable, x="Date")
     
-    for fig in [fig_city, fig_payment]:
-        fig.update_layout(margin=dict(l=0, r=20, t=20, b=20), height=200, template='plotly_dark')
-    fig_product_income.update_layout(margin=dict(l=0, r=0, t=20, b=20), height=500, template='plotly_dark')
+    for fig in [fig_city, fig_payment, fig_gender, fig_income_date]:
+        fig.update_layout(margin=dict(l=0, r=0, t=20, b=20), height=200, template='minty')
     
-    return fig_city, fig_payment, fig_product_income
+    fig_product_income.update_layout(margin=dict(l=0, r=0, t=20, b=20), height=500, template='minty')
+    
+    return fig_city, fig_payment, fig_gender, fig_income_date, fig_product_income
 
 
 # =========== Run Server =========== #
