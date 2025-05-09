efe567f7d3a41ee823833b043c6d32082002ba75
Wellinton Oliveira <86848665+tomosantos@users.noreply.github.com>
2025-05-09 00:44:12 +0000

    finish olist-freight v1

--- yukio/projects/olist-freight-regression/v1/predict.py
+++ yukio/projects/olist-freight-regression/v1/predict.py
@@ -1,8 +1,25 @@
 # %%
 import pandas as pd
+import numpy as np
 import os
 
 base_dir = 'data/'
+model = pd.read_pickle('model/xgb.pkl')
+model
 
+# %%
+df = pd.read_csv(os.path.join(base_dir, 'df_final.csv'))
+df
 
+# %%
+X = df[model['features']]
+predict = model['model'].predict(X)
+predict
+
+# %%
+df['freight-pred'] = predict
+df
+
+# %%
+df[['price', 'distance', 'freight_value', 'freight-pred']]
 # %%
